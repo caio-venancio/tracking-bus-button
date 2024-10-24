6e80592a984ef644e3971ab62c1da6334b216c50
caio-venancio <caio.venancio784@gmail.com>
2024-10-24 14:22:40 -0300

    Molhou

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ kivy.require('2.3.0') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+
+class MyGridLayout(GridLayout):
+     def __init__(self, **kwargs):
+        super(MyGridLayout, self).__init__(**kwargs) #o que significa ess código
+        self.col = 3
+        self.rows = 3
 
 class MyCustomButton(Widget):
     pass
 
-class MyApp(App):
+class TrackingApp(App):
     def build(self):
-        return MyCustomButton()
-
+        self.submit = Button(text="O Ônibus Chegou", font_size=40) 
+        # self.add_widget(self.submit)
+        self.title = 'Tracking Bus App'
+        return self.submit
+        
 
 if __name__ == '__main__':
-    MyApp().run()
+    TrackingApp().run()
